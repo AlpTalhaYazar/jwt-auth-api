ba3c62f35e555ba17c4af2fb90a65d89d00d7cd6
Alp Talha Yazar <AlpTalhaYazar@gmail.com>
2024-12-18 00:34:49 +0300

    feat: update server port to 3000 and enhance login functionality with JWT token generation

--- controllers/main.js
+++ controllers/main.js
@@ -1,7 +1,21 @@
 import { StatusCodes } from "http-status-codes";
+import { createCustomError } from "../errors/custom-error.js";
+import jwt from "jsonwebtoken";
 
 const login = async (req, res) => {
-  res.send("Login route");
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    throw createCustomError("Please provide username and password", 400);
+  }
+
+  const id = new Date().getDate();
+
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+
+  res.status(StatusCodes.OK).json({ msg: "user created", token });
 };
 
 const dashboard = async (req, res) => {

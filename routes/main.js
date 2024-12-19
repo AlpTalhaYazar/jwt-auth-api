e2326491ac9ba88cb2f8ff1e5381b811c76ea325
Alp Talha Yazar <AlpTalhaYazar@gmail.com>
2024-12-19 12:07:36 +0300

    feat: add authentication middleware to dashboard route

--- routes/main.js
+++ routes/main.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { login, dashboard } from "../controllers/main.js";
+import { authentificationMiddleware } from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.route("/dashboard").get(dashboard);
+router.route("/dashboard").get(authentificationMiddleware, dashboard);
 router.route("/login").post(login);
 
 export { router as mainRouter };

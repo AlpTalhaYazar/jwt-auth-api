80eba1f8bd9708c6c8917aad95b847f7e5709a5a
Alp Talha Yazar <AlpTalhaYazar@gmail.com>
2024-12-19 12:08:40 +0300

    feat: refactor dashboard controller to use req.user for username and improve error handling in login

--- controllers/main.js
+++ controllers/main.js
@@ -1,12 +1,15 @@
+import jwt from "jsonwebtoken";
 import { StatusCodes } from "http-status-codes";
 import { createCustomError } from "../errors/custom-error.js";
-import jwt from "jsonwebtoken";
 
 const login = async (req, res) => {
   const { username, password } = req.body;
 
   if (!username || !password) {
-    throw createCustomError("Please provide username and password", 400);
+    throw createCustomError(
+      "Please provide username and password",
+      StatusCodes.BAD_REQUEST
+    );
   }
 
   const id = new Date().getDate();
@@ -19,24 +22,10 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
-  const authHeader = req.headers.authorization;
-
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    throw createCustomError("Invalid token", 401);
-  }
-
-  const token = authHeader.split(" ")[1];
-
-  const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-  if (!decoded) {
-    throw createCustomError("Invalid token", 401);
-  }
-
   const luckyNumber = Math.floor(Math.random() * 100);
 
   res.status(StatusCodes.OK).json({
-    msg: `Hello, ${decoded.username}`,
+    msg: `Hello, ${req.user.username}`,
     secret: `Your lucky number is ${luckyNumber}`,
   });
 };
